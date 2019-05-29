4ad4d25957eba45da303c39cd8823457d88acf65
Ismail Aksoy <iaksoy@seanapse.io>
2019-05-29 22:30:22 +0300

    react add user

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import UsersList from './components/UsersList'
+import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
     constructor() {
@@ -23,10 +24,12 @@ class App extends Component {
             <section className="section">
                 <div className="container">
                     <div className="columns">
-                        <div className="column is-one-third">
+                        <div className="column is-one-half">
                             <br/>
                             <h1 className="title is-1">All Users</h1>
                             <hr/><br/>
+                            <AddUser/>
+                            <br/><br/>
                             <UsersList users={this.state.users}/>
                         </div>
                     </div>
